f48add61e3141284583f13933db560ef8775ffe4
aqp <alvquipom@gmail.com>
2024-02-21 18:08:12 +0100

    Reminders Progress

--- src/pages/user-panel/reminders/new-reminder/NewReminder.js
+++ src/pages/user-panel/reminders/new-reminder/NewReminder.js
@@ -25,13 +25,28 @@ import SignalCellular3BarIcon from "@mui/icons-material/SignalCellular3Bar";
 import SignalCellularConnectedNoInternet4BarIcon from "@mui/icons-material/SignalCellularConnectedNoInternet4Bar";
 import dateFormat from "../../../../utils/dateFormat";
 import dayjs from "dayjs";
+import WorkIcon from "@mui/icons-material/Work";
+import PersonIcon from "@mui/icons-material/Person";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import SchoolIcon from "@mui/icons-material/School";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import FlightIcon from "@mui/icons-material/Flight";
+import FamilyRestroomIcon from "@mui/icons-material/FamilyRestroom";
+import { useLoading } from "../../../../context/loading/LoadingProvider";
+import { Timestamp } from "firebase/firestore";
+import Alert from "@mui/material/Alert";
+import { useDialog } from "../../../../context/dialog/DialogProvider";
 
 export default function NewReminder() {
   const { t } = useTranslation();
   const { language } = useLanguage();
-  const { postReminder } = useReminders();
+  const { setIsLoading } = useLoading();
+  const { openDialog } = useDialog();
+  const { postReminder, getAllReminders } = useReminders();
   const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const [notes, setNotes] = useState("");
+  const [category, setCategory] = useState("");
   const [priority, setPriority] = useState("");
   const [reminderDate, setReminderDate] = useState(null);
   const [reminderTime, setReminderTime] = useState(null);
@@ -39,7 +54,8 @@ export default function NewReminder() {
 
   function handleReset() {
     setTitle("");
-    setDescription("");
+    setNotes("");
+    setCategory("");
     setPriority("");
     setReminderDate(null);
     setReminderTime(null);
@@ -48,145 +64,245 @@ export default function NewReminder() {
   async function handleSubmit(e) {
     e.preventDefault();
     setValidForm(true);
+    setIsLoading(true);
+
+    if (
+      title === "" ||
+      category === "" ||
+      priority === "" ||
+      reminderDate === null ||
+      reminderTime === null
+    ) {
+      setValidForm(false);
+      setIsLoading(false);
+      return;
+    }
 
     const reminder = {
       title,
-      description,
+      notes,
+      category,
       priority,
-      reminderDate,
-      reminderTime,
+      date: Timestamp.fromDate(reminderDate.toDate()),
+      time: reminderTime.format("HH:mm"),
     };
 
     try {
-      console.log(reminder);
+      await postReminder(reminder);
+      await getAllReminders();
+      openDialog({
+        title: t("reminders.reminder-success"),
+        description: t("reminders.reminder-added"),
+        severity: "success",
+      });
     } catch (error) {
       console.error(error);
+      openDialog({
+        title: t("reminders.reminder-error"),
+        description: t("reminders.reminder-not-added"),
+        severity: "error",
+      });
+    } finally {
+      setIsLoading(false);
+      handleReset();
     }
   }
 
   return (
-    <form className={styles["new-reminder-form"]} onSubmit={handleSubmit}>
-      <TextField
-        required
-        id="title"
-        label={t("reminders.reminder-title")}
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        variant="filled"
-        sx={{ mb: 4, background: "#fff" }}
-      />
+    <>
+      <form className={styles["new-reminder-form"]} onSubmit={handleSubmit}>
+        {!validForm && (
+          <Alert severity="error" sx={{ mb: 4 }}>
+            {t("new-project.please-fill-all-fields")}
+          </Alert>
+        )}
 
-      <TextField
-        id="description"
-        label={t("reminders.reminder-description")}
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        variant="filled"
-        sx={{ mb: 4, background: "#fff" }}
-      />
-
-      <FormControl variant="filled" sx={{ background: "#fff" }} required>
-        <InputLabel id="priority-select-label">
-          {t("reminders.reminder-priority")}
-        </InputLabel>
-        <Select
-          labelId="priority-select-label"
-          id="priority-label"
+        <TextField
           required
-          value={priority}
-          label={t("new-project.project-priority")}
-          onChange={(e) => setPriority(e.target.value)}
-        >
-          <MenuItem value="low">
-            <span className={styles["low-priority-text"]}>
-              <SignalCellular1BarIcon
-                className={styles["priority-icon-low"]}
-                sx={{ fontSize: "1.2rem" }}
-              />
-              {t("reminders.priority-low")}
-            </span>
-          </MenuItem>
-          <MenuItem value="medium">
-            <span className={styles["medium-priority-text"]}>
-              <SignalCellular2BarIcon
-                className={styles["priority-icon-medium"]}
-                sx={{ fontSize: "1.2rem" }}
-              />
-              {t("reminders.priority-medium")}
-            </span>
-          </MenuItem>
-          <MenuItem value="high">
-            <span className={styles["high-priority-text"]}>
-              <SignalCellular3BarIcon
-                className={styles["priority-icon-high"]}
-                sx={{ fontSize: "1.2rem" }}
-              />
-              {t("reminders.priority-high")}
-            </span>
-          </MenuItem>
-          <MenuItem value="critical">
-            <span className={styles["critical-priority-text"]}>
-              <SignalCellularConnectedNoInternet4BarIcon
-                className={styles["priority-icon-critical"]}
-                sx={{ fontSize: "1.2rem" }}
-              />
-              {t("reminders.priority-critical")}
-            </span>
-          </MenuItem>
-        </Select>
-      </FormControl>
-
-      <div className={styles["date-time-container"]}>
-        <FormControl variant="standard" sx={{ width: "48%" }}>
-          <LocalizationProvider
-            dateAdapter={AdapterDayjs}
-            adapterLocale={dayjs.locale(language)}
-          >
-            <DatePicker
-              labelId="reminder-date-label"
-              id="reminder-date"
-              inputFormat={dateFormat()}
-              label={t("reminders.reminder-date")}
-              value={reminderDate}
-              onChange={(date) => setReminderDate(date)}
-              slotProps={{ textField: { variant: "filled" } }}
-              sx={{ background: "#fff" }}
-            />
-          </LocalizationProvider>
-        </FormControl>
-
-        <FormControl variant="standard" sx={{ width: "48%" }}>
-          <LocalizationProvider dateAdapter={AdapterDayjs} locale={language}>
-            <TimePicker
-              labelId="reminder-time-label"
-              id="reminder-time"
-              ampm={false}
-              value={reminderTime}
-              label={t("reminders.reminder-time")}
-              onChange={(time) => setReminderTime(time)}
-              slotProps={{ textField: { variant: "filled" } }}
-              sx={{ background: "#fff" }}
-            />
-          </LocalizationProvider>
-        </FormControl>
-      </div>
-
-      <div className={styles.buttonsContainer}>
-        <CustomButton
-          text={t("button.reset")}
-          variant="outlined"
-          color="error"
-          icon={<RestartAltOutlinedIcon />}
-          onClick={handleReset}
+          id="title"
+          label={t("reminders.reminder-title")}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          sx={{ mb: 4, background: "#fff" }}
         />
-        <CustomButton
-          type="submit"
-          text={t("button.submit")}
-          variant="contained"
-          color="primary"
-          icon={<CheckCircleOutlineOutlinedIcon />}
+
+        <TextField
+          multiline
+          id="notes"
+          label={t("reminders.reminder-notes")}
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+          sx={{ background: "white", mb: "2rem" }}
         />
-      </div>
-    </form>
+
+        <div className={styles["category-priority-container"]}>
+          <FormControl sx={{ background: "#fff" }} required>
+            <InputLabel id="category-select-label">
+              {t("reminders.category")}
+            </InputLabel>
+            <Select
+              labelId="category-select-label"
+              id="category-label"
+              required
+              value={category}
+              label={t("new-project.category")}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <MenuItem value="work">
+                <span className={styles["category-text"]}>
+                  <WorkIcon sx={{ fontSize: "1.2rem" }} />
+                  {t("reminders.category-work")}
+                </span>
+              </MenuItem>
+              <MenuItem value="personal">
+                <span className={styles["category-text"]}>
+                  <PersonIcon sx={{ fontSize: "1.2rem" }} />
+                  {t("reminders.category-personal")}
+                </span>
+              </MenuItem>
+              <MenuItem value="health">
+                <span className={styles["category-text"]}>
+                  <LocalHospitalIcon sx={{ fontSize: "1.2rem" }} />
+                  {t("reminders.category-health")}
+                </span>
+              </MenuItem>
+              <MenuItem value="education">
+                <span className={styles["category-text"]}>
+                  <SchoolIcon sx={{ fontSize: "1.2rem" }} />
+                  {t("reminders.category-education")}
+                </span>
+              </MenuItem>
+              <MenuItem value="finance">
+                <span className={styles["category-text"]}>
+                  <AccountBalanceIcon sx={{ fontSize: "1.2rem" }} />
+                  {t("reminders.category-finance")}
+                </span>
+              </MenuItem>
+              <MenuItem value="shopping">
+                <span className={styles["category-text"]}>
+                  <ShoppingCartIcon sx={{ fontSize: "1.2rem" }} />
+                  {t("reminders.category-shopping")}
+                </span>
+              </MenuItem>
+              <MenuItem value="travel">
+                <span className={styles["category-text"]}>
+                  <FlightIcon sx={{ fontSize: "1.2rem" }} />
+                  {t("reminders.category-travel")}
+                </span>
+              </MenuItem>
+              <MenuItem value="family">
+                <span className={styles["category-text"]}>
+                  <FamilyRestroomIcon sx={{ fontSize: "1.2rem" }} />
+                  {t("reminders.category-family")}
+                </span>
+              </MenuItem>
+            </Select>
+          </FormControl>
+
+          <FormControl sx={{ background: "#fff" }} required>
+            <InputLabel id="priority-select-label">
+              {t("reminders.reminder-priority")}
+            </InputLabel>
+            <Select
+              labelId="priority-select-label"
+              id="priority-label"
+              required
+              value={priority}
+              label={t("new-project.project-priority")}
+              onChange={(e) => setPriority(e.target.value)}
+            >
+              <MenuItem value="low">
+                <span className={styles["low-priority-text"]}>
+                  <SignalCellular1BarIcon
+                    className={styles["priority-icon-low"]}
+                    sx={{ fontSize: "1.2rem" }}
+                  />
+                  {t("reminders.priority-low")}
+                </span>
+              </MenuItem>
+              <MenuItem value="medium">
+                <span className={styles["medium-priority-text"]}>
+                  <SignalCellular2BarIcon
+                    className={styles["priority-icon-medium"]}
+                    sx={{ fontSize: "1.2rem" }}
+                  />
+                  {t("reminders.priority-medium")}
+                </span>
+              </MenuItem>
+              <MenuItem value="high">
+                <span className={styles["high-priority-text"]}>
+                  <SignalCellular3BarIcon
+                    className={styles["priority-icon-high"]}
+                    sx={{ fontSize: "1.2rem" }}
+                  />
+                  {t("reminders.priority-high")}
+                </span>
+              </MenuItem>
+              <MenuItem value="critical">
+                <span className={styles["critical-priority-text"]}>
+                  <SignalCellularConnectedNoInternet4BarIcon
+                    className={styles["priority-icon-critical"]}
+                    sx={{ fontSize: "1.2rem" }}
+                  />
+                  {t("reminders.priority-critical")}
+                </span>
+              </MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className={styles["date-time-container"]}>
+          <FormControl variant="standard">
+            <LocalizationProvider
+              dateAdapter={AdapterDayjs}
+              adapterLocale={dayjs.locale(language)}
+            >
+              <DatePicker
+                labelId="reminder-date-label"
+                id="reminder-date"
+                inputFormat={dateFormat()}
+                label={t("reminders.reminder-date")}
+                value={reminderDate}
+                onChange={(date) => setReminderDate(date)}
+                slotProps={{ textField: { required: true } }}
+                sx={{ background: "#fff" }}
+              />
+            </LocalizationProvider>
+          </FormControl>
+
+          <FormControl variant="standard">
+            <LocalizationProvider dateAdapter={AdapterDayjs} locale={language}>
+              <TimePicker
+                labelId="reminder-time-label"
+                id="reminder-time"
+                ampm={false}
+                value={reminderTime}
+                label={t("reminders.reminder-time")}
+                onChange={(time) => setReminderTime(time)}
+                slotProps={{ textField: { required: true } }}
+                sx={{ background: "#fff" }}
+              />
+            </LocalizationProvider>
+          </FormControl>
+        </div>
+
+        <div className={styles.buttonsContainer}>
+          <CustomButton
+            text={t("button.reset")}
+            variant="outlined"
+            color="error"
+            icon={<RestartAltOutlinedIcon />}
+            onClick={handleReset}
+          />
+          <CustomButton
+            type="submit"
+            text={t("button.submit")}
+            variant="contained"
+            color="primary"
+            icon={<CheckCircleOutlineOutlinedIcon />}
+          />
+        </div>
+      </form>
+    </>
   );
 }
--- src/pages/user-panel/reminders/reminders-container/Reminders.js
+++ src/pages/user-panel/reminders/reminders-container/Reminders.js
@@ -3,15 +3,8 @@ import styles from "./Reminders.module.css";
 import UserPanelHeader from "../../../../components/ui/header/user-panel-header/UserPanelHeader";
 import { useTranslation } from "react-i18next";
 import { useReminders } from "../../../../context/reminders/RemindersProvider";
-import {
-  List,
-  ListItem,
-  Checkbox,
-  IconButton,
-  ListItemText,
-} from "@mui/material";
 import NewReminder from "../new-reminder/NewReminder";
-import DeleteIcon from "@mui/icons-material/Delete";
+import CustomScrollableTabs from "../../../../components/ui/tabs/CustomScrollableTabs";
 
 export default function Reminders() {
   const { t } = useTranslation();
@@ -20,29 +13,22 @@ export default function Reminders() {
   return (
     <>
       <UserPanelHeader title={t("user-panel-sidebar.to-do-list")} />
-      <NewReminder />
+
       <div className={styles["reminders-container"]}>
-        {reminders.length > 0 ? (
-          <List>
-            {reminders.map((reminder) => (
-              <ListItem key={reminder.id} className={styles["reminder-item"]}>
-                <Checkbox
-                  checked={reminder.checked}
-                  onChange={() => {}}
-                  inputProps={{ "aria-label": "primary checkbox" }}
-                />
-                <ListItemText primary={reminder.title} />
-                <IconButton edge="end" aria-label="delete" onClick={() => {}}>
-                  <DeleteIcon />
-                </IconButton>
-              </ListItem>
-            ))}
-          </List>
-        ) : (
-          <p className={styles["no-reminders"]}>
-            {t("reminders.no-reminders")}
-          </p>
-        )}
+        <CustomScrollableTabs
+          initialTabId={1}
+          tabs={[
+            {
+              id: 1,
+              name: (
+                <div className={styles["new-reminder-div"]}>
+                  {t("reminders.new-reminder")}
+                </div>
+              ),
+              content: <NewReminder />,
+            },
+          ]}
+        />
       </div>
     </>
   );
